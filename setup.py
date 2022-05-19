c50a5b6e41df9414a2b4c2a9d5f26682cc44e58f
Roskiy <88959563+RoskiyADR@users.noreply.github.com>
2022-05-19 14:23:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text(encoding="utf8")
 setup(
 	name="JustifyDiscord",
 	version="1.0",
-	author="MagMigo / Reidy and community",
+	author="MagMigo / Roskiy and community",
 	description="A debug discord util",
 	long_description=README,
 	long_description_content_type="text/markdown",
